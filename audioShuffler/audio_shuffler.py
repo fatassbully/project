5c4ef8703eb797417f26c73f5f5668da78f70aaf
kkk <kli-kli@tut.by>
2019-05-21 14:08:59 +0300

    dasdasdasds

--- audioShuffler/audio_shuffler.py
+++ audioShuffler/audio_shuffler.py
@@ -1,49 +1,49 @@
 import os
 import random
 import pydub
-# v2.1
+# v2.2
 
 
-def slice_n_dice(linktoaudio="", shuffle=True, number_of_slices=30, percentage=0, times=1):
-    # Changing directory to the directory of audio file
-    chislo = 0
+def slice_n_dice(link_to_audio="", shuffle=True, number_of_slices=30, percentage=0, times=1):
 
     try:
-        typeofaudio = pydub.utils.mediainfo(linktoaudio)['codec_name']
-    except Exception:
-        raise Exception('I can not get the file')
+        type_of_audio = pydub.utils.mediainfo(link_to_audio)['codec_name']
+    except BaseException:
+        raise BaseException('I can not get the file')
 
-    for symbol in linktoaudio[::-1]:
-
-        if symbol == "\\":
-            break
-        else:
-            chislo += 1
-    end = len(linktoaudio) - chislo
-    fordirectory = linktoaudio[:end]
-    os.chdir(fordirectory)
+    # Changing directory to the directory of audio file
+    for_directory = os.path.dirname(link_to_audio)
+    os.chdir(for_directory)
 
     # Check for Number of Repeats
     if times < 1:
-        times = 1
-        print("The number of repeats can't be less than 1")
+        print("The number of repeats can't be less than 1.")
+        print("Setting the number to 1.")
 
     # Creating blank list
     list_of_snippets = []
 
     # Opening file to slice
-    if typeofaudio == "wav":
-        file = pydub.AudioSegment.from_wav(linktoaudio)
-    elif typeofaudio == "mp3":
-        file = pydub.AudioSegment.from_mp3(linktoaudio)
+    wav_formats = ("wav","pcm_s8le","pcm_s16le","pcm_s24le","pcm_s32le")
+
+    if type_of_audio in wav_formats:
+        audio = pydub.AudioSegment.from_wav(link_to_audio)
+    elif type_of_audio == "mp3":
+        print("MP3")
+        audio = pydub.AudioSegment.from_mp3(link_to_audio)
     else:
-        print("Fignia kakaia to")
+        final_file = pydub.AudioSegment.from_mp3("K:\Python_Courses\Wav_shuffler\Error.mp3")
+        return final_file.export('Result.mp3', format='mp3'), True
+
+
+
 
     # Check if number of slices exceeds the length of file
-    if number_of_slices <= file.__len__():
-        step = file.__len__() // number_of_slices
+    audio_length = len(audio)
+    if number_of_slices <= audio_length:
+        step = audio_length // number_of_slices
     else:
-        step = random.choice(range(file.__len__()))
+        step = random.choice(range(audio_length))
 
     # Starting point for slicing
     start = 0
@@ -51,7 +51,7 @@ def slice_n_dice(linktoaudio="", shuffle=True, number_of_slices=30, percentage=0
     # 'For' cycle that slices the file and puts them in list
     for i in range(number_of_slices):
 
-        snippet = file[start:start+step]
+        snippet = audio[start:start+step]
         list_of_snippets.append(snippet)
         start += step
 
@@ -59,15 +59,10 @@ def slice_n_dice(linktoaudio="", shuffle=True, number_of_slices=30, percentage=0
     if shuffle:
         random.shuffle(list_of_snippets)
     else:
-        list_of_snippets = list_of_snippets
+        pass
 
-    # Creation or opening of Temp File
-    if os.path.isfile('Temp.wav'):
-        final_file = pydub.AudioSegment.silent(duration=0)
-    else:
-        s = open('Temp.wav', 'x')
-        s.close()
-        final_file = pydub.AudioSegment.silent(duration=0)
+    # Creation of Final File
+    final_file = pydub.AudioSegment.silent(duration=0)
 
     # Gluing all back together
     for i in list_of_snippets:
@@ -78,11 +73,12 @@ def slice_n_dice(linktoaudio="", shuffle=True, number_of_slices=30, percentage=0
             else:
                 final_file += i
 
-    if typeofaudio == "wav":
+    if type_of_audio == "wav":
         final_file.export('Result.mp3', format='wav')
-        os.remove('Temp.wav')
         return True
     else:
         final_file.export('Result.mp3', format='mp3')
-        os.remove('Temp.wav')
         return True
+
+
+slice_n_dice("K:\Python_Courses\Wav_shuffler\\voice.wav", True, 50, 50, 5)
